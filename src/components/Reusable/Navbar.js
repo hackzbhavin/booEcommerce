691261fac660c40d72f4fe152481635b5e2f638b
Bhavin Patil <56113845+hackzbhavin@users.noreply.github.com>
2021-06-08 21:34:30 +0530

    Update Navbar.js

--- src/components/Reusable/Navbar.js
+++ src/components/Reusable/Navbar.js
@@ -19,7 +19,6 @@ export default function NavbarNav() {
             <Nav.Link href="/products">Products</Nav.Link>
             <Nav.Link href="/contact">Contact</Nav.Link>
             <Nav.Link href="/about">About</Nav.Link>
-         <Nav.Link href="/about">Test</Nav.Link>
             <Nav.Link href="/"><FaCartArrowDown className="cart-icon snipcart-checkout" /></Nav.Link>
   
           
